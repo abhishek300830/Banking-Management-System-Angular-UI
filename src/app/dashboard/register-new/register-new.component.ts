6e001c533aca8209abcd76176512e12dbe4aae2f
abhishek300830 <abhishek300830@gmail.com>
2023-12-09 23:18:08 +0530

    change password and register customer

--- src/app/dashboard/register-new/register-new.component.ts
+++ src/app/dashboard/register-new/register-new.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { DashboardService } from '../service/dashboard.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register-new',
   templateUrl: './register-new.component.html',
-  styleUrls: ['./register-new.component.scss']
+  styleUrls: ['./register-new.component.scss'],
 })
 export class RegisterNewComponent {
+  currentTheme: String;
+  themeSubscription: Subscription;
 
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit() {
+    this.themeSubscription = this.dashboardService.currentTheme$.subscribe(
+      (theme) => {
+        this.currentTheme = theme;
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
+  }
 }
