8fb125e711c077f11d5a4700fdccc88f5c3d04cc
abhishek300830 <abhishek300830@gmail.com>
2023-12-09 16:06:43 +0530

    added change password UI

--- src/app/dashboard/change-password/change-password.component.ts
+++ src/app/dashboard/change-password/change-password.component.ts
@@ -1,10 +1,32 @@
+import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DashboardService } from '../service/dashboard.service';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.scss']
+  styleUrls: ['./change-password.component.scss'],
 })
 export class ChangePasswordComponent {
+  currentTheme: String;
+  themeSubscription: Subscription;
 
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit() {
+    this.themeSubscription = this.dashboardService.currentTheme$.subscribe(
+      (theme) => {
+        this.currentTheme = theme;
+      }
+    );
+  }
+
+  onSubmit(changePasswordForm: NgForm) {
+    console.log(changePasswordForm);
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
+  }
 }
