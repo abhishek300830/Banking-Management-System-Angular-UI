f16ba5036804a8981d8fe955ce2f6302e8723598
abhishek300830 <abhishek300830@gmail.com>
2023-12-15 11:45:16 +0530

    added filter to requests

--- src/app/dashboard/manager-view/requests/requests.component.ts
+++ src/app/dashboard/manager-view/requests/requests.component.ts
@@ -11,6 +11,7 @@ import { ToastService } from 'src/app/shared/toast.service';
 export class RequestsComponent {
   currentTheme: String;
   themeSubscription: Subscription;
+  filteredRequests: string = '';
   // currentPage: number = 1;
 
   modificationRequests: [];
